37e78b1c22b3f82ecfe0e84f79f715177c1eff82
Anshika <anshika2011087@akgec.ac.in>
2023-08-20 00:31:10 +0530

    database connected

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 
 
 //routes
 
 app.get('/', (req, res) => (
-    res.send('Hello World!')
+    res.send('Hello World! yo yo')
 ))
 
 app.listen(5173, () => (
     console.log('Server running on port 5173')
 ))
 
+mongoose.
+connect('mongodb+srv://admin:Anshika123@cluster0.1kmx2xf.mongodb.net/Node-API?retryWrites=true&w=majority')
+.then(()=>(
+    console.log('DB Connected')
+)).catch((error)=>(
+    console.log(error)
+))
