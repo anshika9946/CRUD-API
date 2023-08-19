8440757ef0f7ca0da1a73eb914b5eab18cec9bc7
Anshika <anshika2011087@akgec.ac.in>
2023-08-20 00:48:35 +0530

    database setup

--- server.js
+++ server.js
@@ -5,18 +5,19 @@ const app = express()
 
 //routes
 
-app.get('/', (req, res) => (
+app.get('/', (req, res) => {
     res.send('Hello World! yo yo')
-))
+})
 
-app.listen(5173, () => (
-    console.log('Server running on port 5173')
-))
+
 
 mongoose.
 connect('mongodb+srv://admin:Anshika123@cluster0.1kmx2xf.mongodb.net/Node-API?retryWrites=true&w=majority')
-.then(()=>(
+.then(()=>{
     console.log('DB Connected')
-)).catch((error)=>(
+    app.listen(5173, () => {
+        console.log('Server running on port 5173')
+    })  
+}).catch((error)=>(
     console.log(error)
 ))
