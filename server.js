23aaf57960ad31825854561871f8ea529e966583
Anshika <anshika2011087@akgec.ac.in>
2023-08-22 21:11:44 +0530

    creating model for database

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const Product = require('./modules/productModule')
 const app = express()
 
+
+app.use(express.json())
 
 //routes
 
@@ -9,7 +12,28 @@ app.get('/', (req, res) => {
     res.send('Hello World! yo yo')
 })
 
+app.get('/product', async(req, res) => {
+    try{
+        const product = await Product.find({});
+        res.status(200).json(product);
+        }catch(err){
+        res.status(500).json({message:err.message})
+    }
+})
 
+app.get('/blog', (req, res) => {
+    res.send('this is my blog')
+})
+
+app.post('/product', async(req, res) => {
+    try{
+        const product = await Product.create(req.body)
+        res.status(200).json(product);
+        }catch(err){
+        console.log(err.message);
+        res.status(500).json({message:err.message})
+    }
+})
 
 mongoose.
 connect('mongodb+srv://admin:Anshika123@cluster0.1kmx2xf.mongodb.net/Node-API?retryWrites=true&w=majority')
