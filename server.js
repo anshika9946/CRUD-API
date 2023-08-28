adf2360211270141391eee33c8685232e44a09f4
Anshika <anshika2011087@akgec.ac.in>
2023-08-28 18:45:41 +0530

    fetching single product from database

--- server.js
+++ server.js
@@ -12,20 +12,31 @@ app.get('/', (req, res) => {
     res.send('Hello World! yo yo')
 })
 
-app.get('/product', async(req, res) => {
+app.get('/products/:id', async(req, res) => {
     try{
-        const product = await Product.find({});
+        const{id}=req.params;
+        const product = await Product.findById(id);
         res.status(200).json(product);
-        }catch(err){
-        res.status(500).json({message:err.message})
-    }
-})
+
+        }catch(err){ 
+            res.status(500).json({message:err.message})
+        }}) 
 
 app.get('/blog', (req, res) => {
     res.send('this is my blog')
 })
 
-app.post('/product', async(req, res) => {
+
+app.get('/products', async(req, res) => {
+    try{
+        const products = await Product.find({});
+        res.status(200).json(products);
+        }catch(err){
+        res.status(500).json({message:err.message})
+    }
+})
+
+app.post('/products', async(req, res) => {
     try{
         const product = await Product.create(req.body)
         res.status(200).json(product);
@@ -33,7 +44,9 @@ app.post('/product', async(req, res) => {
         console.log(err.message);
         res.status(500).json({message:err.message})
     }
-})
+})          
+
+
 
 mongoose.
 connect('mongodb+srv://admin:Anshika123@cluster0.1kmx2xf.mongodb.net/Node-API?retryWrites=true&w=majority')
